389685b9d3d122e1a7e0c9bc38a13fb7543272f4
Ankit BagheL <ak9664773@gmail.com>
2023-11-02 13:34:21 +0530

    add login verfication and jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import userModel from "../models/userModel.js";
-import { hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+import JWT from "jsonwebtoken"
 
 export const registerController = async (req, resp) => {
     try {
@@ -36,6 +37,60 @@ export const registerController = async (req, resp) => {
         resp.status(500).send({
             success: false,
             message: `Erorr in Registration`,
+            error
+        })
+    }
+}
+
+//POST LOGIN
+export const loginController = async (req, resp) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return resp.status(404).send({
+                success: false,
+                message: "Invalid email or password"
+            })
+        }
+
+        //Check User
+        const user = await userModel.findOne({ email });
+        if (!user) {
+            return resp.status(404).send({
+                success: false,
+                message: "Email is not registered"
+            })
+        }
+
+        const match = await comparePassword(password, user.password);
+        if (!match) {
+            return resp.status(200).send({
+                success: false,
+                message: "Inavalid Password"
+            })
+        }
+
+        //token
+        const token = await JWT.sign({ _id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '7d',
+        })
+        resp.status(200).send({
+            success: true,
+            message: "Login successfully",
+            user: {
+                name: user.name,
+                email: user.email,
+                phone: user.phone,
+                address: user.address,
+            },
+            token
+        })
+
+    } catch (error) {
+        resp.status(500).send({
+            success: false,
+            message: "Error in login",
             error
         })
     }
